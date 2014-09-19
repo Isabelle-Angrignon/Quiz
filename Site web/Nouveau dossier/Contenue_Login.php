34cc3bae4111bc67bf5c1a6ce3256c5ffd06cfae
Ariekor <851s2001@gmail.com>
2014-09-19 08:54:47 -0400

    Border-radius Mathieu

---
+++ Site web/Nouveau dossier/Contenue_Login.php
@@ -0,0 +1,26 @@
+<div id="alerte" class="Alerte">
+			Vous êtes-tu a Lionnel ? Non ben CIAO ! Oui entrez votre mot de passe S.V.P  
+		</div>
+		<div id="login">
+			<p id="titreConnexion">Connexion</p>
+			
+			<form id="login_Form">
+				<div id="loginLabels">
+					<p id="nomUsager">Nom d'usager </p>
+					<p id="motDePasse">Mot de passe </p>
+				</div>
+				
+				<div id="loginTextFields">
+					<input type="text" id="TBNomUsager" />
+					<input type="text" id="TBMotDePasse" />
+				</div>
+				
+				<input type="submit" id="btnConnexion" value="Se connecter"/>
+			</form>
+
+			
+			<p id="linkLostAcount"><a href="#">Mot de passe oublié ? </a></p>
+			
+			
+		
+		</div>
---
+++ Site web/Nouveau dossier/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<?php 
+		include("Vue/PHP de base/InclusionTemplate.php");
+		include("Vue/PHP de base/InclusionJQuery.php");
+	?>
+	<link rel="stylesheet" href="CSS/Login.css" type="text/css" media="screen" >
+</head>
+
+<body>
+
+	<?php
+		include("Vue/PHP de base/EnteteSite.php");
+		include("Vue/PHP de base/MenuProf.php");
+	?>
+	
+	<div class="contenu">
+	<?php
+		include("Contenue_Login.php");
+	?>
+	</div>
+	
+	<?php
+		include("Vue/PHP de base/BasDePage.php");
+	?>
+
+</body>
+
+</html>
