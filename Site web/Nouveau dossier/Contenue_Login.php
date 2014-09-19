42f7c184c1ae031256b5870ae581bbc66f05cc4b
Ariekor <851s2001@gmail.com>
2014-09-19 10:09:17 -0400

    Login still WIP

--- Site web/Nouveau dossier/Contenue_Login.php
+++ Site web/Nouveau dossier/Contenue_Login.php
@@ -1,15 +1,3 @@
-<?php
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=projetquiz', 'root', '');
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-}
-
-
-?>
 
 <div id="alerte" class="Alerte">
 			Vous êtes-tu a Lionnel ? Non ben CIAO ! Oui entrez votre mot de passe S.V.P  
