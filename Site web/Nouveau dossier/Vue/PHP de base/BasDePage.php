45f2f105608df084ae38a5fe46225e31cbf29d8d
Ariekor <851s2001@gmail.com>
2014-09-19 08:59:59 -0400

    index.html is now index.php

--- Site web/Nouveau dossier/Vue/PHP de base/BasDePage.php
+++ Site web/Nouveau dossier/Vue/PHP de base/BasDePage.php
@@ -1,7 +1,7 @@
 <div id="bdp">
 	<div>
-		<p id="bdpAPropos"><a>À propos</a></p>
-		<p id="bdpFAQ"><a>FAQ</a></p>
-		<p id="bdpDevelopeur"><a>Dévelopeur</a></p>
+		<p id="bdpAPropos"><a href="#">À propos</a></p>
+		<p id="bdpFAQ"><a href="#">FAQ</a></p>
+		<p id="bdpDevelopeur"><a href="#">Dévelopeur</a></p>
 	</div>
 </div>
