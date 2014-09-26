56505d3ba28229fe75fe873de44b692a62d29d49
Ariekor <851s2001@gmail.com>
2014-09-26 09:30:46 -0400

    Fuck May It work

--- Site web/Nouveau dossier/GererClasse.php
+++ Site web/Nouveau dossier/GererClasse.php
@@ -16,7 +16,7 @@
 	?>
 	
 	<div class="contenu">
-		<form action="upload_file.php" method="post"
+		<form action="GestionFichier.php" method="post"
 		enctype="multipart/form-data">
 			<label for="file">Filename:</label>
 			<input type="file" name="file" id="file" accept=".csv"><br>
