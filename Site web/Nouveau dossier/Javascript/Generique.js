d33224be965192722c9de74b387e226f870f9751
Isabelle-Angrignon <isabelle.angrignon@hotmail.ca>
2014-10-01 08:27:48 -0400

    generique.js modif
    modifié ajouteroptionselect, ajouté un param

--- Site web/Nouveau dossier/Javascript/Generique.js
+++ Site web/Nouveau dossier/Javascript/Generique.js
@@ -9,12 +9,13 @@
 // Date : 15/09/2014
 // Intrant(s) : String idSelect, String element 
 // Extrant(s) : Il n'y a pas d'extrant
-// Description : Cette fonction prend l'id d'une balise Select et lui ajoute (en tant qu'attribut value) l'élément passé en paramêtre à l'aide d'une balise option
-function ajouterOption_ToSelect(idSelect, element) {
+// Description : Cette fonction prend l'id d'une balise Select et lui ajoute le texte à afficher ainsi que
+// l'élément "unique" en tant qu'attribut value(passés en paramètre) à l'aide d'une balise option.
+function ajouterOption_ToSelect(idSelect, element, texte) {
 	var optionTag = document.createElement("option");
 	optionTag.Value = element;
 	// createTextNode permet de mettre du texte représentant notre balise (ce que l'usager voit comme texte)
-	optionTag.appendChild(document.createTextNode(element));
+	optionTag.appendChild(document.createTextNode(texte));
 	document.getElementById(idSelect).appendChild(optionTag);
 }
 
