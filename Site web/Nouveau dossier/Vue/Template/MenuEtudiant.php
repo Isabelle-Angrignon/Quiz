0d4e817f104125e042a740eccfbe3d8255570483
Le serveur <kingbaub3@gmail.com>
2014-10-08 14:25:07 -0400

    redirect etudiant

--- Site web/Nouveau dossier/Vue/Template/MenuEtudiant.php
+++ Site web/Nouveau dossier/Vue/Template/MenuEtudiant.php
@@ -1,5 +1,5 @@
 <nav class="fixed">
-	<a href='Vue/Pages/Etudiant-Accueil.php' class='NavBar'>Mes quiz</a>
+	<a href='Etudiant-Accueil.php' class='NavBar'>Mes quiz</a>
 	<a href='#' class='NavBar'>Mon profil</a>
 	<a href='#' class='NavBar'>Mes stats</a>
 </nav>
