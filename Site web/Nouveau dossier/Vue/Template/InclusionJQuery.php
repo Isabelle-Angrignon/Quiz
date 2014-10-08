4aa6dd554d535f3868509654033ce5ca81ee474c
Isabelle Angrignon <isabelle.angrignon@gmail.com>
2014-10-08 14:23:29 -0400

    poussiere

--- Site web/Nouveau dossier/Vue/Template/InclusionJQuery.php
+++ Site web/Nouveau dossier/Vue/Template/InclusionJQuery.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="jquery-ui-1.11.1.custom/jquery-ui.theme.css" type="text/css" media="all" >
-<link rel="stylesheet" href=jquery-ui-1.11.1.custom/jquery-ui.structure.css" type="text/css" media="all" >
+<link rel="stylesheet" href="jquery-ui-1.11.1.custom/jquery-ui.structure.css" type="text/css" media="all" >
 <script src="Javascript/jquery.min.js"></script>
 <script src="jquery-ui-1.11.1.custom/jquery-ui.min.js"></script>
 
