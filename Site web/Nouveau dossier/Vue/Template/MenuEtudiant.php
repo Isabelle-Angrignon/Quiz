27bac4938f4025b5355879fae405e0c431312907
Ariekor <851s2001@gmail.com>
2014-10-14 07:16:12 -0400

    Menu changer

--- Site web/Nouveau dossier/Vue/Template/MenuEtudiant.php
+++ Site web/Nouveau dossier/Vue/Template/MenuEtudiant.php
@@ -1,5 +1,5 @@
 <nav class="fixed">
 	<a href='Etudiant-Accueil.php' class='NavBar'>Mes quiz</a>
-	<a href='#' class='NavBar'>Mon profil</a>
+	<a href='../../GererSonCompte.php' class='NavBar'>Mon profil</a>
 	<a href='#' class='NavBar'>Mes stats</a>
 </nav>
--- Site web/Nouveau dossier/Vue/Template/MenuProf.php
+++ Site web/Nouveau dossier/Vue/Template/MenuProf.php
@@ -1,8 +1,9 @@
 
 <nav class="fixed">
 	<a href='GererQuiz' class='NavBar'>Mes quiz et questions</a>
-	<a href='Cours' class='NavBar'>Mes cours</a>
+	<a href='../../GererCours.php' class='NavBar'>Mes cours</a>
     <a href='Statistiques' class='NavBar'>Statistiques</a>
+    <a href="../../GererSonCompte.php" class="NavBar">Mon Compte</a>
     <?php
         // Ajout de l'onglet de menu Admin
         if(isset($_SESSION['TypeUsager']) && $_SESSION['TypeUsager'] == 'Admin') {
