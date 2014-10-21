6ae39eb44c06933bf6aa4992b56721664a82eabb
Ariekor <851s2001@gmail.com>
2014-10-21 17:20:26 -0400

    Changement thème

--- Site web/Nouveau dossier/Vue/Template/InclusionJQuery.php
+++ Site web/Nouveau dossier/Vue/Template/InclusionJQuery.php
@@ -1,7 +1,7 @@
 <script src="sweetalert-master/lib/sweet-alert.js"></script>
 <link rel="stylesheet" type="text/css" href="sweetalert-master/lib/sweet-alert.css">
-<link rel="stylesheet" href="jquery-ui-1.11.1.custom/jquery-ui.theme.css" type="text/css" media="all" >
-<link rel="stylesheet" href="jquery-ui-1.11.1.custom/jquery-ui.structure.css" type="text/css" media="all" >
+<link rel="stylesheet" href="jquery-ui-1.11.2.custom/jquery-ui.theme.css" type="text/css" media="all" >
+<link rel="stylesheet" href="jquery-ui-1.11.2.custom/jquery-ui.structure.css" type="text/css" media="all" >
 <script src="Javascript/jquery.min.js"></script>
 <script src="jquery-ui-1.11.1.custom/jquery-ui.min.js"></script>
 
