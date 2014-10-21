86eaa97b85e23d04115958d93a4388a56af64de6
Ariekor <851s2001@gmail.com>
2014-10-21 08:16:19 -0400

    Reignite

--- Site web/Nouveau dossier/Vue/Template/EnteteSite.php
+++ Site web/Nouveau dossier/Vue/Template/EnteteSite.php
@@ -1,4 +1,8 @@
 <div id="enteteSite" class="fixed">
 	<img class="logo" src="Vue/Images/Logo_QIz.png" alt="logo">
 	<p id="nomSite"> QUIZINFO</p>
+    <?php
+    if (isset($_SESSION['idUsager'])){
+        echo '<p id="NomUsager">'.$_SESSION['Nom'] . ' ' . $_SESSION['Prenom'] .'</p> <div id ="BTN_Deconnecter">Se Deconnecter</div> ';
+    }?>
 </div>
