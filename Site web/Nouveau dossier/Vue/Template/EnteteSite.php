577575f45cff9ae9b500247cea86a7b0591c04f7
Ariekor <851s2001@gmail.com>
2014-10-21 09:14:14 -0400

    Deconnecter marche :D

--- Site web/Nouveau dossier/Vue/Template/EnteteSite.php
+++ Site web/Nouveau dossier/Vue/Template/EnteteSite.php
@@ -1,10 +1,16 @@
 <div id="enteteSite" class="fixed">
 	<img class="logo" src="Vue/Images/Logo_QIz.png" alt="logo">
 	<p id="nomSite"> QUIZINFO</p>
-    <div id="EspaceDeconnexion">
+    <form id="EspaceDeconnexion" method="post" action="controleur/seDeconnecter.php">
     <?php
     if (isset($_SESSION['idUsager'])){
         echo '<div id="NomUsager">'.$_SESSION['Nom'] . ' ' . $_SESSION['Prenom'] .'</div> <div id ="BTN_Deconnecter">Se Deconnecter</div> ';
     }?>
-    </div>
+    </form>
 </div>
+
+<script>
+    $('#BTN_Deconnecter').click(function(){
+        $('#EspaceDeconnexion').submit();
+    });
+</script>
