ddd32c3d028a03cd9fc2888f9ef9e1ecc06968da
Ariekor <851s2001@gmail.com>
2014-10-24 15:45:26 -0400

    admin avancer

--- Site web/Nouveau dossier/Admin.php
+++ Site web/Nouveau dossier/Admin.php
@@ -8,6 +8,9 @@
     demarrerSession();
     redirigerSiNonConnecte('Admin');
     ?>
+    <link rel="stylesheet" href="Vue/CSS/Admin.css" type="text/css" media="screen" >
+    <link rel="stylesheet" href="Vue/CSS/GererSonCompte.css" type="text/css" media="screen" >
+    <script src="Javascript/Admin.js"></script>
 </head>
 
 <body>
@@ -20,11 +23,34 @@ include("Vue/Template/MenuProf.php");   // ou MenuEtudiant.php
 
 ?>
 
-<div class="contenu">
+<div class="contenu" >
+    <div id="leConteneur">
+    <div id="optionCours"class="Liste">
+        <label>Options</label>
+        <ul id="optionAdmin">
+            <li id="reintinialiser" class="ui-state-default padding">Réinitialiser un mot de passe</li>
+            <li id="ajouterProf" class="ui-state-default padding nonImplementer">Ajouter un compte professeur</li>
+            <li id="modifierCours" class="ui-state-default padding nonImplementer">Supprimer un professeur</li>
+            <li id="nommerAdmin" class="ui-state-default padding nonImplementer">Nommer un nouvel administateur</li>
+            <li id="modifierCours" class="ui-state-default padding nonImplementer">Modifier un cours</li>
+        </ul>
 
-Page Admin
+    </div>
+
+    </div>
 
 </div>
+
+<script>
+    $('.nonImplementer').click(function(){
+        swal("Désolé","Cette fonction n'est pas encore implémentées","error");
+    });
+    //Vue/Template/dynamique-ReinitialiserMotDePasse.php
+    $('#reintinialiser').click(function(){
+        CreerDeploiement('Vue/Template/dynamique-ReinitialiserMotDePasse.php');
+    })
+
+</script>
 
 <?php
 include("Vue/Template/BasDePage.php");
