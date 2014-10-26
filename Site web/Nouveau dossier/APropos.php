2211eecf70126779c77f61426a86c658cb343b5d
Isabelle-Angrignon <isabelle.angrignon@hotmail.ca>
2014-10-26 12:30:02 -0400

    amelioré texte...

--- Site web/Nouveau dossier/APropos.php
+++ Site web/Nouveau dossier/APropos.php
@@ -18,39 +18,35 @@ if(isset($_SESSION['typeUsager']))
 {
     if($_SESSION['typeUsager'] == 'Prof' ||$_SESSION['typeUsager'] == 'Admin')
     {
-        include("Vue/Template/MenuProf.php");   // ou MenuEtudiant.php
+        include("Vue/Template/MenuProf.php"); 
     }
     else
     {
         include("Vue/Template/MenuEtudiant.php");
     }
 }
-
 ?>
 
 <div class="contenu">
     <div class = "centrer">
-        <h1 id="header"> Le projet Quiz </h1>
+        <h1 id="header"> Le projet QuizInfo </h1>
         <img id="logo" src="Vue/Images/Logo_QIz.png" alt="logo" >
         <p class="centrer">
             Le QuizInfo est un outil de génération de quiz avec une base de données contenant
-            entre autre une banque de questions de plusieurs types : des questions à choix de réponse,
-            réponses multiples et questions à vrai ou faux.
+            entre autre une banque de questions de plusieurs types, entre autre des questions
+            à choix de réponse unique, à réponses multiples et des questions de type vrai ou faux.
         </p >
         <p class="centrer">
             Cet outil comporte plusieurs interfaces dont une pour les professeurs et les administrateurs
-            et une autre pour les étudiants. Le QuizInfo pourrait particulièrement être utilisé pour permettre aux
-            étudiants de première année de se pratiquer afin d’augmenter leur potentiel de réussite et réduire les abandons du programme.
-            Les enseignants créent des questions et les agences en quiz à soumettre aux étudiants.
+            et une autre pour les étudiants. Le QuizInfo sera particulièrement utile pour les étudiants
+            de première année qui pourront s'En servir pour se pratiquer et ainsi augmenter leur
+            potentiel de réussite et réduire les abandons du programme.  Les enseignants créent
+            des questions et les agencent en quiz à soumettre aux étudiants.
         </p>
-
     </div>
-
 </div>
 
-<?php
-include("Vue/Template/BasDePage.php");
-?>
+<?php  include("Vue/Template/BasDePage.php");  ?>
 
 </body>
 
