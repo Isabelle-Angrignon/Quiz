7483ccd7c4a933ff3c59050387f5fad91c053958
Ariekor <851s2001@gmail.com>
2014-10-29 13:50:54 -0400

    favicon done

--- Site web/Nouveau dossier/Vue/Template/InclusionTemplate.php
+++ Site web/Nouveau dossier/Vue/Template/InclusionTemplate.php
@@ -1,6 +1,8 @@
 <link rel="stylesheet" href="Vue/CSS/Template.css" type="text/css" media="screen" >
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" >
 <script src="Javascript/Generique.js"></script>
+<link rel="icon"
+      href="Vue/Images/favicon.ico">
 <title>QuizInfo.com</title>
 
 <?php
