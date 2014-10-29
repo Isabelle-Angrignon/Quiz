5485ef76675c7293bd309095ba4b67e823148dff
Ariekor <851s2001@gmail.com>
2014-10-29 13:08:39 -0400

    Modif mineure

--- Site web/Nouveau dossier/Vue/Template/InclusionTemplate.php
+++ Site web/Nouveau dossier/Vue/Template/InclusionTemplate.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="Vue/CSS/Template.css" type="text/css" media="screen" >
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" >
 <script src="Javascript/Generique.js"></script>
+<title>QuizInfo.com</title>
 
 <?php
 include("Modele/ModeleUtilisateurs.php");
