a573ff443f41e76785dcbbdb59e41e6dfa038354
Le serveur <kingbaub3@gmail.com>
2014-11-04 11:25:41 -0500

    nommer-admin

---
+++ Site web/Nouveau dossier/Vue/dynamique-NommerAdmin.php
@@ -0,0 +1,34 @@
+<?php
+?>
+
+
+
+    <p class="titre">Nommer un nouvel Admin</p>
+    <hr>
+    <div class="conteneur" >
+        <div class="gauche">
+            DA du professeur : <br>
+        </div>
+
+        <div class="droite">
+            <input type="text" id="TB_DA" /><br>
+        </div>
+    </div>
+    <div id="soumettre" class="ListeDivElementStyle JquerryButton">Rendre administrateur</div>
+
+
+
+<script>
+    $('#soumettre').button();
+    $('#soumettre').click(function(){
+        if($('#TB_DA').val().length != 0 && $('#TB_DA').val().length <= 10 && $('#TB_DA').val()[0] == "4") {
+
+           // reinitialiserMotDePasse($('#TB_DA').val());
+
+        }
+        else{
+            swal({title:"Erreur" ,type:"warning", text:"Le numéro de DA n'est pas conforme"});
+        }
+    });
+
+</script>
