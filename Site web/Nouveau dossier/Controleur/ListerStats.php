fff9e405c7583ffba69ce1ae212114e32e1785a7
Ariekor <851s2001@gmail.com>
2014-11-12 15:20:17 -0500

    Controleur done sauf utilitaire

--- Site web/Nouveau dossier/Controleur/ListerStats.php
+++ Site web/Nouveau dossier/Controleur/ListerStats.php
@@ -1,5 +1,17 @@
 
 <?php
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  ListetStats.php
+//  Fait par : Simon Bouchard
+//  Commenter le : 12/11/2014
+//
+//  Description :
+//  Télécharge un fichier CSV contenant tout les stats
+//
+//  Retour : CSV
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
 header( 'Content-Type:charset=UTF-8');
 include("Utilitaires.php");
 include("../Modele/ModeleUtilisateurs.php");
@@ -9,17 +21,17 @@ demarrerSession();
 redirigerSiNonConnecte('Prof');
 header('Content-Disposition: attachment;Filename=QuizInfoStats.csv');
 $stats  = obtenirStat();
-echo date("Y-m-d");
+echo date("Y-m-d"); // La date de création du fichier comme première ligne du fichier
 echo "\n";
 $Key = array_keys($stats[0]);
-for ( $i = 0 ; $i < count($Key); $i++)
+for ( $i = 0 ; $i < count($Key); $i++)      // Crée un header pour les colonnes
 {
     if ($i%2 == 0 ) {
         echo $Key[$i] . ";";
     }
 }
 echo  "\n";
-for ($i = 0 ; $i < count($stats); $i++)
+for ($i = 0 ; $i < count($stats); $i++)         // Boucle pour remplir les stats
 {
     for ($j = 0 ; $j < count($stats[$i])/2; $j++) {
         echo $stats[$i][$j] . ";";
--- Site web/Nouveau dossier/Controleur/SupprimerUnCompte.php
+++ Site web/Nouveau dossier/Controleur/SupprimerUnCompte.php
@@ -1,5 +1,19 @@
 <?php
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  SupprimerUnCompte.php
+//  Fait par : Simon Bouchard
+//  Commenter le : 12/11/2014
+//
+//  Description :
+//  Ce fichier change le e-mail d'un usager suite a un appel de type post afin de supprimer un compte
+//
+//  Parametre en post :
+//  numeroDA = Le numero de DA de l'étudiant que l'on souhaite supprimer
+//
+//  Retour : Nombre de lignes affecté par les changement dans la BD
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
 include("Utilitaires.php");
 include("../Modele/ModeleUsagers.php");
 include("../Modele/ModeleUtilisateurs.php");
