e6e1dd3ad7d95cd9806044da3f3f1f6bc66ee08b
Isabelle-Angrignon <isabelle.angrignon@hotmail.ca>
2014-11-18 12:03:16 -0500

    Commentaires

--- Site web/Nouveau dossier/Controleur/FonctionQuizEtudiant/ListerQuizFormatifs.php
+++ Site web/Nouveau dossier/Controleur/FonctionQuizEtudiant/ListerQuizFormatifs.php
@@ -1,5 +1,21 @@
 <?php
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  SetIdCoursSession.php
+//  Fait par : Isabelle Angrignon
+//  Commenté le : 18/11/2014
+//
+//  But : Met la variable identifiant le idCours passé en POST dans la variable de session pour réféerence future
+//        Remet ensuite les variables de session liées au Quiz à "vide"
+//
+//  POST: 'selectCours' = le idCours
+//
+//  Session :  'idCours' : Le nom le dit
+//
+//  Sortie :  int 1|0 pour identifier si un cours est sélectionné ou non.  Le menu cours ayant l'item "Tous les cours"
+//            en position "0".
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
 include("..//Utilitaires.php");
 include("..//..//Modele/ModeleUtilisateurs.php");
 include("..//..//Modele/ModeleUsagers.php");
--- Site web/Nouveau dossier/Controleur/FonctionQuizEtudiant/verifierSiQuizTermine.php
+++ Site web/Nouveau dossier/Controleur/FonctionQuizEtudiant/verifierSiQuizTermine.php
@@ -1,10 +1,22 @@
 <?php
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  verifierSiQuizTermine.php
+//  Fait par : Isabelle Angrignon
+//  Commenté le : 18/11/2014
+//
+//  But : Vérifie s'il reste des questions da la liste de question du quiz en cours
+//
+//  Session :  'listeQuestions' = liste de idQuestions pour le quiz en cours
+//
+//  Sortie :  int 1|0
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
 include("..//Utilitaires.php");
 demarrerSession();
 redirigerSiNonConnecte('Etudiant');
 
-
+// cette variable de session contient une liste de idQuestions pour le quiz en cours
 if (!empty($_SESSION['listeQuestions']))
 {
     echo '0';
