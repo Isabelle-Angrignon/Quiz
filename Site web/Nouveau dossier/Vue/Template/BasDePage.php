ab455e775fb82bccca359554af46d6058be7d911
Ariekor <851s2001@gmail.com>
2014-11-19 10:49:38 -0500

    petite faute corriger

--- Site web/Nouveau dossier/Vue/Template/BasDePage.php
+++ Site web/Nouveau dossier/Vue/Template/BasDePage.php
@@ -2,7 +2,7 @@
         <nav id="navbdp">
 		<a href="APropos.php" class="NavBar">À propos</a>
 		<a href="FAQ.php" class="NavBar">F.A.Q.</a>
-        <a href="Developeur.php" class="NavBar">Dévelopeur</a>
+        <a href="Developeur.php" class="NavBar">Développeur</a>
         <a href="https://docs.google.com/document/d/1F_JO7jholgO1Yg9DbdP0nlx3YrRiEFGJEE0jzbvhEqs/edit"
         class="NavBar" target="_blank">Documentation</a>
         </nav>
