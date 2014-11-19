32d78ab726f104b5767a83c6f6e01c442fb7faf2
Ariekor <851s2001@gmail.com>
2014-11-19 10:47:02 -0500

    BDP final

--- Site web/Nouveau dossier/Vue/Template/BasDePage.php
+++ Site web/Nouveau dossier/Vue/Template/BasDePage.php
@@ -1,7 +1,9 @@
 <div id="bdp">
-        <nav>
-		<a href="APropos.php" class="navbar">À propos</a>
-		<a href="FAQ.php" class="navbar">F.A.Q.</a>
-        <a href="Developeur.php" class="navbar">Dévelopeur</a>
+        <nav id="navbdp">
+		<a href="APropos.php" class="NavBar">À propos</a>
+		<a href="FAQ.php" class="NavBar">F.A.Q.</a>
+        <a href="Developeur.php" class="NavBar">Dévelopeur</a>
+        <a href="https://docs.google.com/document/d/1F_JO7jholgO1Yg9DbdP0nlx3YrRiEFGJEE0jzbvhEqs/edit"
+        class="NavBar" target="_blank">Documentation</a>
         </nav>
 </div>
