50a322292e5fff74ee9fc17ee4afd97fed65b7d5
Isabelle-Angrignon <isabelle.angrignon@hotmail.ca>
2014-12-01 16:38:11 -0500

    info pour stats

--- Site web/Nouveau dossier/Controleur/ListerStats.php
+++ Site web/Nouveau dossier/Controleur/ListerStats.php
@@ -21,6 +21,33 @@ demarrerSession();
 redirigerSiNonConnecte('Prof');
 header('Content-Disposition: attachment;Filename=QuizInfoStats.csv');
 $stats  = obtenirStat();
+
+echo 'POUR UTILISER LES STATS:
+***Nous n\'utilisons pas les parmetres d\'importation de csv par defauts d\'Excel.
+***Veuillez suivre les etapes suivantes pas a pas.
+***Les accents sont volontairement omis pour faciliter la comprehension de ceux qui
+***n\'utilisent pas cette procedure.
+
+Ouvrir un fichier vide Excel.
+Cliquer sur l\'onglet "Donnees".
+Puis sur l\'outil "A partir du texte".
+Selectionner le fichier "QuizInfoStats.csv" le plus recent et "Importer".
+Il devrait etre dans votre dossier "Telechargements".
+Selectionner "Delimite".
+Choisir a quelle ligne vous voulez commencer l\'importation selon si vous voulez enlever les instructions en entete.
+Choisir "Origine du fichier:"  "65001: Unicode (UTF-8)".
+Cliquer "Suivant".
+Choisir "Separateurs  -Autre:" et mettre le symbole ¦  (ctrl+alt+7 sur clavier "Francais(Canada)" ).
+Cliquer "Suivant".
+A l\'etape 3 , vous pouvez donner un type de donnee a vos colonnes si necessaire.
+Par defaut, tout est standard donc les nombres seront consideres comme tel et vous pourrez faire des formules.
+Cliquer "Terminer" puis "OK".
+
+POUR FILTRER ET TRIER VOS DONNEES:
+Dans l\'onglet "Donnees", selectionnez la ligne de titre (ligne 4) et cliquez sur "Filtrer".
+Utilisez ensuite les menus deroulants dans chaque entete de colonne pour filtrer et trier selon vos besoins.
+';
+
 echo date("Y-m-d"); // La date de création du fichier comme première ligne du fichier
 echo "\n";
 $Key = array_keys($stats[0]);
